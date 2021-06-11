6aab2a6c193089f3410f6a1a2c3682dbbbd1aaa2
rtkleong10 <rtkleong10@gmail.com>
2021-06-11 22:31:28 +0800

    [update] Save previous links using cookies

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
+
+const SHORTENED_LINKS_COOKIE_NAME = "shortend-links";
+
+function setCookie(cname, cvalue, exdays) {
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+	var expires = "expires=" + d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+	var name = cname + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
 function copyFunc(e) {
 	e.classList.add("shorten-copied");
 	var tempInput = document.createElement("input");
@@ -9,6 +35,7 @@ function copyFunc(e) {
 }
 
 window.onload = () => {
+	// Menu toggle
 	document.getElementById("mobile-menu-toggle").onchange = e => {
 		let isChecked = e.target.checked;
 
@@ -19,12 +46,26 @@ window.onload = () => {
 		}
 	}
 
-	document.getElementById("shorten-original-link").oninput = e => {
-		e.target.parentElement.classList.remove("shorten-error");
-	}
+	// Load previously shortened links from cookie
+	let cookieValue = getCookie(SHORTENED_LINKS_COOKIE_NAME);
+	let shortenedLinks = (cookieValue === "") ? [] : JSON.parse(cookieValue);
 
-	document.getElementById("shorten-original-link").onchange = e => {
-		e.target.parentElement.classList.remove("shorten-error");
+	for (let i = 0; i < shortenedLinks.length; i++) {
+		const {
+			originalLink,
+			shortLink,
+		} = shortenedLinks[i];
+
+		let outputDiv = document.createElement("div");
+		outputDiv.classList.add("shorten-output");
+		outputDiv.innerHTML = `
+			<p class="shorten-long-link">${originalLink}</p>
+			<div>
+				<p class="shorten-short-link">${shortLink}</p>
+				<button class="btn" data-url="${shortLink}" onclick="copyFunc(this)"></button>
+			</div>
+		`;
+		document.getElementById("shorten-outputs").prepend(outputDiv);				
 	}
 
 	document.getElementById("shorten-form").onsubmit = e => {
@@ -33,6 +74,7 @@ window.onload = () => {
 		let originalLink = input.value;
 		input.value = "";
 
+		// Check if the input is empty
 		if (originalLink === "") {
 			input.parentElement.classList.add("shorten-error");
 			return;
@@ -40,6 +82,7 @@ window.onload = () => {
 			input.parentElement.classList.remove("shorten-error");
 		}
 
+		// Creating loading div
 		let outputDiv = document.createElement("div");
 		outputDiv.classList.add("shorten-output");
 		outputDiv.innerHTML = `
@@ -53,6 +96,7 @@ window.onload = () => {
 		})
 			.then(response => response.json())
 			.then(data => {
+				// Update output div
 				let shortLink = data.result.short_link;
 				outputDiv.innerHTML = `
 					<p class="shorten-long-link">${originalLink}</p>
@@ -61,10 +105,27 @@ window.onload = () => {
 						<button class="btn" data-url="${shortLink}" onclick="copyFunc(this)"></button>
 					</div>
 				`;
+
+				// Save to cookie
+				shortenedLinks.push({
+					originalLink,
+					shortLink,
+				});
+
+				setCookie(SHORTENED_LINKS_COOKIE_NAME, JSON.stringify(shortenedLinks), 365);
 			})
-			.catch(() => {
-				alert("shorten-error: We were unable to shorten your link");
-				console.log(shorten - error);
+			.catch(error => {
+				alert("Error: We were unable to shorten your link");
+				console.log(error);
+				document.getElementById("shorten-outputs").removeChild(outputDiv);
 			});
 	};
+
+	document.getElementById("shorten-original-link").oninput = e => {
+		e.target.parentElement.classList.remove("shorten-error");
+	}
+
+	document.getElementById("shorten-original-link").onchange = e => {
+		e.target.parentElement.classList.remove("shorten-error");
+	}
 };
